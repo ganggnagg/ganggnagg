57f0af4b9a06e5b0e070c792a420940cd62777d9
ganggnagg <167389792+ganggnagg@users.noreply.github.com>
2024-06-18 23:13:12 +0900

    Add files via upload

---
+++ number.js
@@ -0,0 +1,12 @@
+var myAge=28;
+var myHeight=178.5;
+console.log(typeof myAge);
+console.log(typeof myHeight);
+
+var kuk=95;
+var eng=85;
+var math=67;
+var total=kuk+eng+math;
+var avg=total/3;
+console.log(total);
+console.log(avg);
---
+++ variable.js
@@ -0,0 +1,8 @@
+var msg='Hello';
+msg=100;
+console.log(msg);
+
+var msg='Hello';
+var count=100;
+msg=count;
+console.log(msg);
